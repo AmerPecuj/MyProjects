b4c7ebeb383e82a2b21297a5699a73efd8d75183
Amer Ali <amer@live.dk>
2015-12-26 00:12:27 +0100

    Commit and push

--- MyProjects/src/HelloWorld.java
+++ MyProjects/src/HelloWorld.java
@@ -1,10 +1,15 @@
 
 public class HelloWorld {
 
+	
+	static int g=2;
+	static int f = 4;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World!");
+		
 
 	}
 
